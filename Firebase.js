aa029c4c25ab8b707d62f3925f5f1781c7494cf5
shuklamanish57738-ctrl <shuklamanish57738@gmail.com>
2026-01-20 23:19:19 +0530

    Initialize Firebase with configuration and exports

---
+++ Firebase.js
@@ -0,0 +1,19 @@
+import firebase from '@react-native-firebase/app';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { auth, firestore, storage };
